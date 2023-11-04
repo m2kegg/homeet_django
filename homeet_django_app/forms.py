428680a1162d4f688fb47b8936ab654f2801257d
m2kegg <58165692+m2kegg@users.noreply.github.com>
2023-11-05 00:01:25 +0300

    форма для 1 страницы

--- homeet_django_app/forms.py
+++ homeet_django_app/forms.py
@@ -1,12 +1,21 @@
 from django import forms
 
+class RadioSelectSex(forms.RadioSelect):
+    def create_option(
+        self, name, value, label, selected, index, subindex=None, attrs=None
+    ):
+        option = super().create_option(self, name, value, label, selected, index, subindex=subindex, attrs=attrs)
+        option['attrs']['id'] = f'{value}'
 
+
+CHOICES =[('sex1', "Парень"), ('sex2', "Девушка")]
 class StudentForm(forms.Form):
-    name_surname = forms.CharField(widget=forms.TextInput(attrs={'class': 'input_name_surname'}))
-    sex = forms.ChoiceField(widget=forms.RadioSelect)
-    date_birth = forms.DateField(widget=forms.DateInput(attrs={'class': 'input_date'}))
-    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'input_name_surname'}))
-    about_yourself = forms.CharField(widget=forms.Textarea(attrs={'class': 'input_about'}))
+    name_surname = forms.CharField(widget=forms.TextInput(attrs={'class': 'input_name_surname', 'id':'name_surname', 'placeholder':''}))
+    sex = forms.ChoiceField(widget=RadioSelectSex,choices=CHOICES)
+    date_birth = forms.DateField(widget=forms.DateInput(attrs={'class': 'input_date', 'type':'date', }))
+    telegram = forms.CharField(widget=forms.TextInput(attrs={'class':'input_name_surname', 'id':'rg', 'placeholder':''}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'input_name_surname', 'id':'number', 'placeholder':''}))
+    about_yourself = forms.CharField(widget=forms.Textarea(attrs={'class': 'input_about', 'id':'about', 'placeholder':''}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
